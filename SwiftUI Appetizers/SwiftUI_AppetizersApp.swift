ad0e262e294113138af729b1329734e1f0b0d7f1
John Lollar <jlollar45@gmail.com>
2021-08-29 17:32:54 -0500

    Initial Commit

---
+++ SwiftUI Appetizers/SwiftUI_AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_AppetizersApp.swift
+//  SwiftUI Appetizers
+//
+//  Created by ParkingPal on 8/29/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
