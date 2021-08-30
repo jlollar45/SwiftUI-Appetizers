1407b19af7161986746ab272891e0bd59de5a97c
John Lollar <jlollar45@gmail.com>
2021-08-29 22:26:30 -0500

    Added appetizer list view model

--- SwiftUI Appetizers/AppetizerListView.swift
+++ SwiftUI Appetizers/AppetizerListView.swift
@@ -8,14 +8,22 @@
 import SwiftUI
 
 struct AppetizerListView: View {
+    
+    @StateObject var viewModel = AppetizerListViewModel()
+    
     var body: some View {
         NavigationView {
-            List(MockData.appetizers) { appetizer in
+            List(viewModel.appetizers) { appetizer in
                 AppetizerListCell(appetizer: appetizer)
             }
             .navigationTitle("Appetizers")
         }
+        .onAppear {
+            viewModel.getAppetizers()
+        }
     }
+    
+    
 }
 
 struct AppetizerListView_Previews: PreviewProvider {
---
+++ SwiftUI Appetizers/AppetizerListViewModel.swift
@@ -0,0 +1,26 @@
+//
+//  AppetizerListViewModel.swift
+//  SwiftUI Appetizers
+//
+//  Created by ParkingPal on 8/29/21.
+//
+
+import SwiftUI
+
+final class AppetizerListViewModel: ObservableObject {
+    
+    @Published var appetizers: [Appetizer] = []
+    
+    func getAppetizers() {
+        NetworkManager.shared.getAppetizer { result in
+            DispatchQueue.main.async {
+                switch result {
+                case .success(let appetizers):
+                    self.appetizers = appetizers
+                case .failure(let error):
+                    print(error.localizedDescription)
+                }
+            }
+        }
+    }
+}
