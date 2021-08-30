af892b5be8991cb0a7172ce557cda9af9de9ecf8
John Lollar <jlollar45@gmail.com>
2021-08-29 21:26:14 -0500

    Added Appetizer Model

---
+++ SwiftUI Appetizers/Appetizer.swift
@@ -0,0 +1,36 @@
+//
+//  Appetizer.swift
+//  SwiftUI Appetizers
+//
+//  Created by ParkingPal on 8/29/21.
+//
+
+import Foundation
+
+struct Appetizer: Decodable {
+    let id: Int
+    let name: String
+    let description: String
+    let price: Double
+    let imageURL: String
+    let protein: Int
+    let calories: Int
+    let carbs: Int
+}
+
+struct AppetizerResponse {
+    let request: [Appetizer]
+}
+
+struct MockData {
+    static let sampleAppetizer = Appetizer(id: 0001,
+                                           name: "Test Appetizer",
+                                           description: "This is the description for my appetizer. It is yummy.",
+                                           price: 9.99,
+                                           imageURL: "",
+                                           protein: 99,
+                                           calories: 99,
+                                           carbs: 99)
+    
+    static let appetizers = [sampleAppetizer, sampleAppetizer, sampleAppetizer, sampleAppetizer]
+}
