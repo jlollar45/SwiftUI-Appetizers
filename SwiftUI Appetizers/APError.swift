acfeb1d2e894e45495621a89b23c8eb82dd6f991
John Lollar <jlollar45@gmail.com>
2021-08-29 22:05:02 -0500

    Added network manager

---
+++ SwiftUI Appetizers/APError.swift
@@ -0,0 +1,15 @@
+//
+//  APError.swift
+//  SwiftUI Appetizers
+//
+//  Created by ParkingPal on 8/29/21.
+//
+
+import Foundation
+
+enum APError: Error {
+    case invalidURL
+    case invalidResponse
+    case invalidData
+    case unableToComplete
+}
